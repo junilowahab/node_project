59cc4dc64f6871ed4502677ce2f1e89421596391
junilowahab <junilojoshuawahab@gmail.com>
2025-06-20 12:09:00 +0100

    Installed cors to allow cross origin requests

--- node_project_local/Backend/index.js
+++ node_project_local/Backend/index.js
@@ -1,5 +1,6 @@
 const express  = require('express');            //Imports express frameworks
 const bodyParser = require('body-parser');      //Helps us read JSON from frontend
+const cors = require('cors')
 const fs = require('fs');                       //Node's built in file system module
 const app = express();                          //Create an express app
 const PORT = 3000;                              //Set the port where the server will run
@@ -8,6 +9,7 @@ const TODOS_FILE = './todos.json'               //Pathe to the file storage
 
 app.use(bodyParser.json())
 app.use(express.static('public'));
+app.use(cors())
 
 
 function getTodos(){
